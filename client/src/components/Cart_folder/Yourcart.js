a1f83cafabb2ce0514faa1e3addae2cf5f730302
Vinit <vinits1014@gmail.com>
2023-04-08 23:52:57 +0530

    Modified the server.js and Yourcart.js file(Second Commit)

--- client/src/components/Cart_folder/Yourcart.js
+++ client/src/components/Cart_folder/Yourcart.js
@@ -9,28 +9,54 @@ const Yourcart = () => {
 
   let { productID } = useParams();
   console.log(productID);
-  const [proID, setproID] = useState([]);
+  const [proID, setproID] = useState({});
+  const [qty, setqty] = useState(0);
+  const fetchData = async () => {
+    const response = await fetch(
+      `http://localhost:8000/productDetails/${productID}`
+    );
+    const json = await response.json();
+    console.log(json[0]);
+    setproID(json[0]);
+    setqty(json[0].quantity);
+  };
   useEffect(() => {
-    fetch(`http://localhost:8000/productDetails/${productID}`)
-      .then((res) => res.json())
-      .then((data) => setproID(data));
-  }, [productID]);
+    fetchData();
+  }, [productID,qty]);
 
-  if (!proID) {
-    return <div>Loading...</div>;
-  }
-
+  // const BuyFunction = async (event) => {
+  //   event.preventDefault();
+  //   alert("Successfully bought");
+  //   await axios.post("http://localhost:8000/productBuy", proID[0])
+  //     .then((response) => {
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error sending product data:", error);
+  //     });
+  // };
   const BuyFunction = async (event) => {
-    event.preventDefault();
-    alert("Successfully bought");
-    await axios.post("http://localhost:8000/productBuy", proID[0])
-      .then((response) => {
-        console.log(response.data);
-
-      })
-      .catch((error) => {
-        console.error("Error sending product data:", error);
-      });
+    console.log(qty);
+    if (qty===1) {
+      console.log("Only 1 item left");
+    }
+    if (qty<=0) {
+      alert("Product out of stock");
+    }
+    else{
+      event.preventDefault();
+      alert("Successfully bought");
+      window.location.reload();
+      await axios
+        .put(`http://localhost:8000/buy/${productID}`, proID)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          //if the PUT request returns a 400 status code, the .catch() block will be executed and the error message "Product out of stock" will be logged to the console.
+          console.error(error.response.data.error);
+        });
+    }
   };
 
   return (
@@ -38,36 +64,35 @@ const Yourcart = () => {
       <Row className={classes.item}>
         <Col sm={6}>
           <div className={classes.divi}>
-            <img src={proID[0]?.imgUrl} alt={proID[0]?.Pname}></img>
+            <img src={proID?.imgUrl} alt={proID?.Pname}></img>
           </div>
         </Col>
         <Col className={classes.info}>
-          <h2>{proID[0]?.Pname}</h2>
+          <h2>{proID?.Pname}</h2>
           <br />
           <br />
-          <p>
-            {proID[0]?.Details}
-          </p>
+          <p>{proID?.Details}</p>
           <Row className={classes.buttonRow}>
             <Col style={{ textAlign: "right" }}>
-              <p>Price: $ {proID[0]?.Price}</p>
+              <p>Price: $ {proID?.Price}</p>
               {/* <form action="" method="POST"> */}
-                <Button onClick={BuyFunction} type="submit" id="buy" variant="primary">
-                  Buy Now
-                </Button>
+              <Button
+                onClick={BuyFunction}
+                type="submit"
+                id="buy"
+                variant="primary"
+              >
+                Buy Now
+              </Button>
               {/* </form> */}
             </Col>
           </Row>
         </Col>
       </Row>
-      <Recommendation 
-        RproductId = {productID}
-      />
+      <Recommendation RproductId={productID} />
     </div>
   );
 };
 export default Yourcart;
 
-
-
-
+// Hello , I am having two tables in mysql database one is productlist and another is buytable , in productlist one column is for quantity and it would be decreasing by 1 for that product as an when user clicks on buy now button and that product if not in buylist would be added using insert query and if already present then just increasing the value of quantity in buytable by 1 . I am using react , nodejs and mysql for this project , Do help me , will I be needed to use put or patch request for the same. Also provide me with an example.
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
   user: "root",
   password: "password",
   database: "ecom",
-})
+});
 
 app.get("/message", (req, res) => {
   connection.connect((err) => {
@@ -34,71 +34,123 @@ app.get("/message", (req, res) => {
   });
 });
 
-app.get("/productDetails/:productID",(req,res)=>{
-
+app.get("/productDetails/:productID", (req, res) => {
   const pd = req.params.productID;
   const query = `SELECT * FROM productlist WHERE Pid = '${pd}'`;
-  connection.connect((err)=>{
+  connection.connect((err) => {
     if (err) {
       console.log(err);
       return;
     }
-    console.log("Data fetched");
-    connection.query(query, (err,results)=>{
+    console.log("Data fetched from get request");
+    connection.query(query, (err, results) => {
       if (err) {
         console.log(err);
       }
       res.json(results);
     });
-  })
-})
+  });
+});
 
-app.get("/recommendInCart/:productID",(req,res)=>{
+app.get("/recommendInCart/:productID", (req, res) => {
   const pd = req.params.productID.charAt(0);
-  
+
   console.log(pd);
   const query = `SELECT * FROM productlist WHERE Pid like '${pd}%'`;
-  connection.connect((err)=>{
+  connection.connect((err) => {
     if (err) {
       console.log(err);
       return;
     }
-    console.log("Data fetched");
-    connection.query(query, (err,results)=>{
+    console.log("Data fetched from recommendate Cart");
+    connection.query(query, (err, results) => {
       if (err) {
         console.log(err);
       }
       res.json(results);
     });
-  })
+  });
+});
 
+app.put("/buy/:productID", (req, res) => {
+  const productID = req.params.productID;
+  console.log(productID);
+  const query = `select * from productlist where pid = '${productID}'`;
+  connection.query(query, (err, results) => {
+    // console.log(results);
+    if (err) {
+      console.log(err);
+    }
+    const id = results[0].Pid;
+    const name = results[0].Pname;
+    const price = results[0].Price;
+    const details = results[0].Details;
+    const imgURL = results[0].imgUrl;
+    const quantity = results[0].quantity;
 
-})
+    console.log(quantity);
+    if (quantity === 0) {
+      console.log("No more products can be bought");
+      // return res.status(400).json({ error: "Product out of stock" });
+    }
+    connection.beginTransaction((err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Server error" });
+      }
+      const updateProductlist = `update productlist set quantity = ${
+        quantity - 1
+      } where pid = '${productID}'`;
+      connection.query(updateProductlist, (err, results) => {
+        if (err) {
+          console.log(err);
+          connection.rollback();
+          return res.status(500).json({ error: "Server error" });
+        }
+      });
 
-app.post("/productBuy",(req,res)=>{
-  const id = req.body.Pid;
-  const name = req.body.Pname;
-  const price = req.body.Price;
-  const details = req.body.Details;
-  const imgURL = req.body.imgUrl;
-  console.log(id);
-  // res.json("successwefd");
-
-  connection.connect((err)=>{
-            if (err) {
-                console.log(err);
-                return;
-            }
-            var sql = "insert into BuyTable values('"+id+"','"+name+"','"+price+"','"+details+"','"+imgURL+"')";
-            connection.query(sql,(err,result)=>{
-                if (err) throw err;
-                console.log(result);
-                // res.send("Item bought successfully "+result);
-            });
-    
-        })
-})
-
+      const checkQuery = `select * from buytable where Bid = "${id}"`;
+      connection.query(checkQuery, (err, results) => {
+        // console.log(results);
+        if (err) throw err;
+        if (results.length === 0) {
+          // Product doesn't exist in buytable, so insert new row
+          const insertQuery =
+            "insert into BuyTable values('" +
+            id +
+            "','" +
+            name +
+            "','" +
+            price +
+            "','" +
+            details +
+            "','" +
+            imgURL +
+            "','" +
+            1 +
+            "')";
+          connection.query(insertQuery, (err, results) => {
+            if (err) throw err;
+            console.log(results);
+            console.log("Product added to buylist");
+          });
+        } else {
+          // Product already exists in buytable, so update quantity
+          const currentQuantity = results[0].quantity;
+          const updateQuery = `UPDATE buytable SET quantity = ${
+            currentQuantity + 1
+          } WHERE Bid = "${id}"`;
+          connection.query(updateQuery, (err, results) => {
+            if (err) throw err;
+            console.log("Quantity updated in buytable");
+            console.log(results);
+          });
+        }
+      });
+    });
+  });
+});
+                                      
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
 });
